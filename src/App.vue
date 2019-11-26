7fa31de9d5eaa3875c0a15c0969ae15a4cf06f01
swoo1226 <copycat1226@gmail.com>
2019-11-26 20:52:22 +0900

    slot을 통하여 컴포넌트를 자유롭게 이동시킴

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <main class="flexbox">
+      <Board id="board-1">
+        <Card id="card-1" draggable="true">
+          <p>Card one</p>
+        </Card>
+      </Board>
+      <Board id="board-2">
+        <Card id="card-2" draggable="true">
+          <p>Card two</p>
+        </Card>
+      </Board>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import Board from './components/Board.vue';
+import Card from './components/Card.vue';
 export default Vue.extend({
   name: 'app',
   components: {
-    HelloWorld
+    Board,
+    Card
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.flexbox {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 768px;
+  height: 100vh;
+  overflow: hidden;
 }
+.flexbox .board {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 300px;
+  background-color: #313131;
+  padding: 15px;
+}
+.flexbox .board .card {
+  padding: 15px 25px;
+  background-color: #f3f3f3;
+  cursor: pointer;
+  margin-bottom: 15px;
+}
+
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <div 
+    class="card"
+    :id="id"
+    :draggable="draggable"
+    @dragstart="dragStart"
+    @dragover.stop
+  >
+  <slot />
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Card",
+    props: ['id', 'draggable'],
+    methods: {
+        dragStart: e => {
+            const target = e.target;
+
+            e.dataTransfer.setData('card_id', target.id);
+
+            setTimeout(() => {
+                target.style.display = "none";
+            }, 0)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
